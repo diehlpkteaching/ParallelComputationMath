2630fb4fc3e5667250ebcdc37016bec6317c9b63
Patrick Diehl <patrickdiehl@lsu.edu>
2019-08-10 15:44:31 -0500

    Update template

--- code/lecture7-loops.cpp
+++ code/lecture7-loops.cpp
@@ -10,9 +10,8 @@ std::cout << i << std::endl;
 
 int main(){
 
+
     std::vector<int> nums(1000000,0);
- 
-    //std::for_each(nums.begin(), nums.end(), print);
 
     std::for_each(std::execution::par_unseq,nums.begin(), nums.end(), print);
 
